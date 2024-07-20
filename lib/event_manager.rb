4c626d9db5c75f3c8ffc68f18b67eb2252c51b95
Gustavo <gustavoooliveiradecarvalho@gmail.com>
2024-07-19 22:17:03 -0400

    feat: make defs to see the most commom hours & week's day

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,6 +2,7 @@
 
 require 'csv'
 require 'erb'
+require 'time'
 require 'colorize'
 require 'google/apis/civicinfo_v2'
 
@@ -16,9 +17,53 @@ def clean_phone_num(phone_num)
   cleaned
 end
 
+def most_commom_hrs!(hrs_hash, dta)
+  h = get_hours dta
+  hrs_hash[h] = 0 unless hrs_hash.key? h
+  hrs_hash[h] += 1
+end
+
+def most_commom_wday!(wday_hash, dta)
+  wd = get_wday dta
+  wday_hash[wd] = 0 unless wday_hash.key? wd
+  wday_hash[wd] += 1
+end
+
+def get_wday(dta)
+  dt = dta.split(' ')
+  date = dt[0].split('/')
+  y = "20#{date[2]}".to_i
+  m = date[0].to_i
+  d = date[1].to_i
+  wday_name Date.new(y, m, d).wday
+end
+
+def wday_name(day)
+  case day
+  when 0 then 'sunday'
+  when 1 then 'monday'
+  when 2 then 'tuesday'
+  when 3 then 'wednesday'
+  when 4 then 'thursday'
+  when 5 then 'friday'
+  else 'saturday' end
+end
+
+def get_hours(dt)
+  dt = dt.split(' ')
+  time = dt[1].split(':')
+  h = time[0]
+  min = time[1]
+  if min.to_i >= 30
+    (h.to_i + 1).to_s
+  else
+    h
+  end
+end
+
 def valid_num?(phone_num)
   return true if phone_num.size == 10
-  
+
   false
 end
 
@@ -57,18 +102,25 @@ contents = CSV.open(
   headers: true,
   header_converters: :symbol
 )
-
+week_days = {}
+hours = {}
 contents.each do |row|
   # id = row[0]
   # name = row[:first_name]
   # zipcode = clean_zipcode row[:zipcode]
+  # phone_num = clean_phone_num(row[:homephone])
+  # valid = valid_num? phone_num
 
-  phone_num = clean_phone_num(row[:homephone])
-  is_valid = valid_num? phone_num
+  date_time = row[:regdate]
+  most_commom_hrs! hours, date_time
+  most_commom_wday! week_days, date_time
 
-  puts "#{phone_num}: #{is_valid}"
   # legislators = legisla tor_by_zipcode zipcode
+  # puts "#{phone_num}: #{is_valid}"
 
   # personal_letter = erb_letter.result(binding)
   # save_letter id, personal_letter
 end
+
+puts week_days
+puts hours
